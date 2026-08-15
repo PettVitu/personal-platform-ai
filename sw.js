0da2574c374efa49a8c6c409b66c20bfe29fccaf
Petterson <petiviturino@gmail.com>
2026-08-14 21:07:18 -0300

    chore: remove legacy static entrypoint

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='vibe-coding-prototype-v1';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
